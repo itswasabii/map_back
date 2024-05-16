a09f3f676e0b89677939edbd7c4c6efa62f2f8f6
Gideon Ogot Junior <juniorgideon99@gmail.com>
2024-05-16 23:38:18 +0300

    new branch

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
     root: true,
     env: { browser: true, es2020: true },
     extends: [
@@ -23,3 +24,25 @@ module.exports = {
       'react/no-unescaped-entities': 'off',
     },
   }
+=======
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
+>>>>>>> b3012764 (new branch)
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
@@ -15,3 +16,15 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ChakraProvider>
   </React.StrictMode>
 );
+=======
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+>>>>>>> b3012764 (new branch)
