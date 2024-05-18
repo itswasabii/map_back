35cae3d85037854b79373efbe11297061f8d4112
wachuka7 <rachaelwachuka7@gmail.com>
2024-05-19 00:08:19 +0300

    feat: add comments and like

--- server/models/post_model.py
+++ server/models/post_model.py
@@ -33,8 +33,22 @@ class Post(db.Model):
     category = db.Column(SqlEnum(PostCategory), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    likes_count = db.Column(db.Integer, default=0)
+    comments_count = db.Column(db.Integer, default=0)
+    shares_count = db.Column(db.Integer, default=0)
 
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    likes= db.relationship('Like', backref='post', lazy='dynamic')
+    shares= db.relationship('Share', backref='post', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super(Post, self).__init__(**kwargs)
+        self.likes_count = 0
+        self.comments_count = 0
+        self.shares_count = 0
+
+    def __repr__(self):
+        return f'<Post {self.post_id}>'
 
 class Comment(db.Model):
     comment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -43,3 +57,21 @@ class Comment(db.Model):
     cohort_id = db.Column(db.Integer, db.ForeignKey('cohort.cohort_id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Like(db.Model):
+    like_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Like {self.like_id}>'
+
+class Share(db.Model):
+    share_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.post_id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Share {self.share_id}>'
